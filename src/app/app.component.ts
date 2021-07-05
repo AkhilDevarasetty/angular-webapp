b2ff146ca3323ff5fdf847d8a6df13f715c3c841
Devarasetty Akhil <m1050751@mindtree.com>
2021-07-05 20:59:42 +0530

    added password toggle functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,28 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
+      username: new FormControl(''),
       password: new FormControl('')
     })
     this.router.navigate(["login"]);
   }
 
-  submittLoginForm(form) {
-    console.log(form.value.email);
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    const togglePassword = document.getElementById('togglePassword');
+    const password = document.getElementById('password');
+    togglePassword.addEventListener('click', (ele) => {
+      let type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+      let src = togglePassword.getAttribute("src") === '/assets/images/eye.svg' ? '/assets/images/eye-clicked.svg' : '/assets/images/eye.svg';
+      togglePassword.setAttribute('src', src);
+    });
   }
+
+  submittLoginForm(form) {
+    console.log(form.value.username);
+  }
+
+
 }
