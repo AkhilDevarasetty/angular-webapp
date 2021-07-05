e5f10a462307ce7b8fdbaf586bf81de80b95bcd9
Devarasetty Akhil <m1050751@mindtree.com>
2021-07-05 20:32:40 +0530

    added login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-webapp';
+  loginForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    })
+    this.router.navigate(["login"]);
+  }
+
+  submittLoginForm(form) {
+    console.log(form.value.email);
+  }
 }
